8e956ea02ce9d621ee48ebe16b6e5d496e0e6370
TerranceBanh <codeberg.currawong@aleeas.com>
2021-08-04 23:04:44 -0700

    Test Service Worker

--- js/service_worker/register.js
+++ js/service_worker/register.js
@@ -4,7 +4,7 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator
       .serviceWorker
-      .register('../../sw1.js')
+      .register('./sw1.js')
       .then(reg => {
         console.log('SW Registered')
         console.log(reg)
